ff85ee11d545283a1c04630371c9c89cc12258ad
Engineer Smith <56016593+EngineerSmith@users.noreply.github.com>
2024-12-13 12:31:35 +0000

    Fixed OS requirement

--- conf.lua
+++ conf.lua
@@ -31,7 +31,7 @@ end
 
 assertFatal(not jit, "Luajit missing", "This game requires luajit - that's packaged with the game.")
 assertFatal(love._version_major ~= 12, "Love version error", "Project requires love 12.")
-assertFatal(jit.os ~= "Windows" and jit.os ~= "Linux", "Incompatible error", "This game does not support this platform: "..jit.os)
+--assertFatal(jit.os ~= "Windows" and jit.os ~= "Linux", "Incompatible error", "This game does not support this platform: "..jit.os)
 
 local settings = require("util.settings")
 local json = require("util.json")
